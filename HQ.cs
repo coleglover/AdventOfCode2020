3cd11b977ec16bc57e453485da3aafc3f0723720
coleglover <cole.r.glover@gmail.com>
2020-12-05 19:00:41 -0800

    completed day one

--- HQ.cs
+++ HQ.cs
@@ -7,9 +7,7 @@ namespace AoC._2020
         static void Main()
         {
             ExpenseReport.NumberCruncher(ExpenseReport.ImportData());
-
             Console.ReadLine();
-
         }
     }
 }
