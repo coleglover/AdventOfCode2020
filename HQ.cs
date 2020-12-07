611c7c20290ad2cbb64247bcc57fe22dd0206115
coleglover <cole.r.glover@gmail.com>
2020-12-06 17:59:03 -0800

    completed day two

--- HQ.cs
+++ HQ.cs
@@ -6,7 +6,13 @@ namespace AoC._2020
     {      
         static void Main()
         {
+            //< Day 01
             ExpenseReport.NumberCruncher(ExpenseReport.ImportData());
+
+            //< Day 02
+            var possibleSledPasswords = TobogganPassword.GetValidPasswords();
+            Console.WriteLine($"\nTotal possible Sled passwords: {possibleSledPasswords[0]}");
+            Console.WriteLine($"Total possible Toboggan passwords: {possibleSledPasswords[1]}");
             Console.ReadLine();
         }
     }
