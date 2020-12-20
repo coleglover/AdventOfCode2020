d7c58c6fbc70f4b4fc0f09dd308ff1d21dbc39f7
Cole Glover <cole.r.glover@gmail.com>
2020-12-19 22:13:52 -0800

    Update BinaryBoarding.cs

--- Classes/BinaryBoarding.cs
+++ Classes/BinaryBoarding.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AoC._2020
 {
@@ -8,9 +9,49 @@ namespace AoC._2020
     {
         public static string[] ImportData()
         {
-            string[] seating = System.IO.File.ReadAllLines(@"#\AoC.2020\InputData\Day05\Day05.Input.txt");
+            return System.IO.File.ReadAllLines(@"C:\Users\Cole\source\repos\AoC.2020\InputData\Day05\Day05.Input.txt");                                        
+        }
 
-            return seating;
+        public static List<int> FindSeatID(IEnumerable<string> boardingPasses) 
+        {
+            //List<int> seatIDs = new List<int>();
+
+            var seatID = boardingPasses.Select(coords => PartitionBoardingPass(coords));
+            //seatIDs.Add((seatID).ToList());
+            List<int> seatIDs = seatID.ToList();
+
+            foreach (int ID in seatIDs) 
+            {
+                Console.WriteLine(ID);
+                break;
+            }          
+
+            return seatIDs;
+        }
+
+        public static int PartitionBoardingPass(string boardingPass) 
+        {
+            var xDir = boardingPass.Substring(0, 7);
+            var yDir = boardingPass.Substring(7);
+
+            Console.WriteLine($"{xDir} {yDir}");
+
+            var row = ParseRowCoord(xDir);
+            var col = ParseColCoord(yDir);
+
+            //return row * col;
+
+            return 2 * 2;
+        }
+
+        public static int ParseRowCoord(string xCoord) 
+        { 
+
+        
+        }
+
+        public static int ParseColCoord(string yCoord)
+        {
 
         }
     }
